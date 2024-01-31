dfc8d626d032c5d2dc571c70e16bee4c11995508
shahfahad19 <sfahad4280@gmail.com>
2024-01-31 15:29:28 +0500

    updating download mechanism

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.get('/api/version', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
