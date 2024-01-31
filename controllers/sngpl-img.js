f4046bbd8524e34061efefe1e2964c8f5bfa10c2
shahfahad19 <sfahad4280@gmail.com>
2024-01-31 15:43:39 +0500

    updating download mechanism

--- controllers/sngpl-img.js
+++ controllers/sngpl-img.js
@@ -1,16 +1,21 @@
 const axios = require('axios');
-const { Readable } = require('stream');
 
 exports.getImage = async (req, res) => {
     try {
         const { ref, month } = req.params;
         const imageUrl = `https://www.sngpl.com.pk/imageservlet?consumer=${ref}&billmon=${month}`;
 
-        // Fetch the image from the external URL and directly pipe the stream to the response
         const response = await axios.get(imageUrl, { responseType: 'stream' });
+
+        const cacheDurationInSeconds = 30 * 24 * 60 * 60; // 1 month
+        const expirationDate = new Date(Date.now() + cacheDurationInSeconds * 1000).toUTCString();
+
+        res.setHeader('Cache-Control', `public, max-age=${cacheDurationInSeconds}`);
+        res.setHeader('Expires', expirationDate);
+
         response.data.pipe(res);
     } catch (error) {
         console.error('Error fetching or serving image:', error);
         res.status(500).send('Internal Server Error');
     }
-}
+};
