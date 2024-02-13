dc0dbf2ae133ee6c6dc9e1b2fd76b0c831b426e7
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2024-02-13 13:21:01 +0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -59,7 +59,7 @@ router.get('/sngpl/:ref', (req, res) => {
 
 router.get('/version', (req, res) => {
     res.json({
-        versionCode: 2,
+        versionCode: 3,
         versionName: '1.1',
         message: 'A new update is available',
         appLink: 'https://google.com',
