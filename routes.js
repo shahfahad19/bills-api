18673b3e4a9c5ff7651eca50863416db99124b79
shahfahad19 <sfahad4280@gmail.com>
2024-03-10 11:45:04 +0500

    update

--- routes.js
+++ routes.js
@@ -17,6 +17,41 @@ router.get('/', (req, res) => {
 router.get('/bill/:ref', (req, res) => {
     const ref = req.params.ref;
     if (ref.length === 14) {
+        return res.json({
+            type: 'electricity',
+            company: 'PESCO',
+            ref: '12345678912345',
+            bill_name: 'Ahmad',
+            units: '31 Units',
+            bill_month: 'March',
+            reading_date: '3 March 2024',
+            current_bill: '982',
+            after_due_bill: '1100',
+            due_date: '25 March 2024',
+            remaining_days: 10,
+            past_data: [],
+            bill_data: ''
+        });
+
+    } else if (ref.length === 11) {
+        return res.json({
+            type: 'gas',
+            company: 'SNGPL',
+            ref: '12345678912',
+            bill_name: 'Zain',
+            units: '0.81 HM3',
+            bill_month: 'March',
+            reading_date: '2 March 2024',
+            current_bill: '1600',
+            after_due_bill: '1700',
+            due_date: '24 March 2024',
+            remaining_days: 10,
+            past_data: [],
+            bill_data: ''
+        });
+    }
+
+    if (ref.length === 14) {
         getElectricityBill(req, res);
     } else if (ref.length === 11) {
         getGasBill(req, res);
@@ -62,7 +97,7 @@ router.get('/version', (req, res) => {
         versionCode: 3,
         versionName: '1.2',
         message: 'A new update is available!',
-        appLink: 'https://github.com/shahfahad19/mybills/releases/download/app/MyBills.v1.2.apk',
+        appLink: 'https://www.upload-apk.com/vjcW9hqIfwJwuH9',
         skipable: false
     });
 });
